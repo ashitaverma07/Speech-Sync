41ccd677310d7c2126aa7228d82dcb7fefcd2381
ashitaverma07 <ashitaverma14@gmail.com>
2024-02-07 22:03:26 +0530

    sixth

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ window.speechSynthesis.onvoiceschanged = () => {
     voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
 
-document.querySelector("button").addEventListener("click", () =>{
+
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
